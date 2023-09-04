57cac2608cd318a8825237d87f83a5697d60afad
Ke Ning <mintnick@gmail.com>
2023-09-04 18:31:02 +0800

    add a few icons, add i18n

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <h1>{{ $t("message.hello") }}</h1>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // VueI18nPlugin({
+    //   include: resolveBaseUrl(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
+    // }),
+  ],
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
