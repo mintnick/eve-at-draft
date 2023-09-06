d0243f56f3419faa3ce78f08b2af3909bfbbe610
Ke Ning <mintnick@gmail.com>
2023-09-06 21:17:38 +0800

    resize images using Python script

--- src/python_scripts/resize.py
+++ src/python_scripts/resize.py
@@ -7,8 +7,34 @@ import os
 
 target_size = (32, 32)
 
-input_dir = "../assets/icon"
-output_dir = "../assets/out"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+input_dir = os.path.abspath(os.path.join(current_dir, "..", "assets", "icons"))
+output_dir = os.path.abspath(os.path.join(current_dir, "..", "assets", "resize"))
 
-if not os.path.exists(output_dir):
-  os.makedirs(output_dir)
+# Function to create directories if they don't exist
+def ensure_directory_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+for root, dirs, files in os.walk(input_dir):
+  for file in files:
+    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+      try:
+        # Create the corresponding output directory structure
+        relative_path = os.path.relpath(root, input_dir)
+        output_subdir = os.path.join(output_dir, relative_path)
+        ensure_directory_exists(output_subdir)
+
+        # Open the image
+        img = Image.open(os.path.join(root, file))
+        
+        # Resize the image to the target size
+        img = img.resize(target_size, Image.ANTIALIAS)
+        
+        # Save the resized image to the output directory
+        output_path = os.path.join(output_subdir, file)
+        img.save(output_path)
+        print(f"Resized and saved: {output_path}")
+      
+      except Exception as e:
+        print(f"Error processing {file}: {str(e)}")
