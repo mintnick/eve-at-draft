de9cb233dc6c5841a645f7f60dc8b82400930909
Ke Ning <mintnick@gmail.com>
2023-09-10 04:11:43 +0800

    update add_ship function.

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,44 @@ import Ship from './components/Ship.vue';
 import { ref, reactive } from 'vue'
 console.log(data)
 // rules
-const max_points = 100,
-      max_ships = 10,
-      max_hull_type = 4,
-      max_flagship = 1,
-      max_logistics = 1,
-      max_ban_hull_type = 3;
+const MAX_POINTS = 100,
+      MAX_SHIPS = 10,
+      MAX_HULL_TYPE = 4,
+      MAX_FLAGSHIP = 1,
+      MAX_LOGISTICS = 1,
+      MAX_BAN_HULL_TYPE = 3;
 
 // data
-let pool = reactive([]),
-    pick = reactive([]),
+let pick = reactive([]),
     ban = reactive([]);
 let total_points = ref(0);
 
-
 // functions
 function add_ship(hull_type, ship_name, property) {
-  console.log(hull_type, ship_name)
+  // same ship + 1 point
+  for (const ship of pick) {
+    if (ship.ship_name == ship_name) {
+      property.points += 1;
+      break;
+    }
+  }
+  for (const ship of pick) {
+    if (ship.ship_name == ship_name) {
+      ship.points += 1;
+    }
+  }
+
+  // add to pick list
   pick.push({
     "ship_name": ship_name,
     "hull_type": hull_type,
-    "points": parseInt(property.points)
+    "points": property.points
   })
-  // pick.sort((a, b) => (a.points > b.points) ? -1 : (b.points > a.points) ? 1 : 0)
+  total_points.value += property.points
+
   pick.sort((a, b) => (b.points - a.points));
+
+  // update limit list
 }
 
 function remove_ship(hull_type, ship_name, property) {
@@ -43,29 +57,36 @@ function ban_ship(hull_type, ship_name, property) {
 <template>
   <h1>EVE AT Draft</h1>
   <h1>{{ total_points }}</h1>
-  <!-- <span>{{ pick }}</span>
-  <br />
-  <span>{{ ban }}</span> -->
 
   <!-- Picks -->
+  <h2>Pick</h2>
   <div v-for="ship in pick">
-    <span>{{ ship.ship_name }} {{ ship.points }}</span>
+    <!-- <span>{{ ship.ship_name }} {{ ship.points }}</span> -->
+    <Ship 
+    :hull_type="ship.hull_type"
+    :ship_name="ship.ship_name"
+    :points="ship.points"
+    />
+    <button @click="remove_ship(hull_type, ship_name, property)">REMOVE</button>
   </div>
-  <!-- Bans -->
+  
   <!-- Pool-->
+  <h2>Pool</h2>
   <div v-for="(ships, hull_type) in data">
     <h3>{{ hull_type }}</h3>
     <div v-for="(property, ship_name) in ships">
       <Ship
       :hull_type = hull_type
       :ship_name = ship_name
-      :property= property
+      :points= property.points
       />
       <button @click="add_ship(hull_type, ship_name, property)">ADD</button>
-      <button @click="remove_ship(hull_type, ship_name, property)">REMOVE</button>
+      <!-- <button @click="remove_ship(hull_type, ship_name, property)">REMOVE</button> -->
       <button @click="ban_ship(hull_type, ship_name, property)">BAN</button>
     </div>
   </div>
+
+  <!-- Bans -->
 </template>
 
 <style scoped>
